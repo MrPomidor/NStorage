9477c70bc68bd784796b5e1688894aba639faaf8
Anton Shyrokykh <anton.black777@gmail.com>
2021-08-03 19:26:09 +0300

    initial commit

---
+++ NStorage/NStorage/BinaryStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NStorage
+{
+    public class BinaryStorage : IBinaryStorage
+    {
+
+        public BinaryStorage(StorageConfiguration configuration)
+        {
+
+        }
+
+        public void Add(string key, Stream data, StreamInfo parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
